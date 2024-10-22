995f448317bb6d7a78ee7df6c76cb63ce7c30ac3
JeredHuck <jeredhuck@gmail.com>
2024-10-21 22:09:22 -0500

    some design choices moving forward

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,12 +1,18 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  topGames:string[] = [
+    'Zelda',
+    'Mario'
+  ]
 
 }
