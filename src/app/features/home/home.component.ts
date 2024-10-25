ba803006725e27ac948e157a1655321819d8a954
JeredHuck <jeredhuck@gmail.com>
2024-10-24 21:24:32 -0500

    erroneous z-index service and finally a solution to the classic navbar shuffle

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,18 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ZIndexService } from '../../core/guards/services/z-index-service';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  zIndex!: number;
 
-  topGames:string[] = [
-    'Zelda',
-    'Mario',
-    'Sonic'
-  ]
+  topGames: string[] = ['Zelda', 'Mario', 'Sonic'];
 
+  constructor(private zIndexService: ZIndexService) {}
+
+  ngOnInit() {
+    this.zIndexService.setZIndex('home', 5);
+    this.zIndex = this.zIndexService.getZIndex('home');
+  }
 }
--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,22 +1,38 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ZIndexService } from '../../../core/guards/services/z-index-service';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, NgStyle],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
-  public sidebarVisibility:boolean = false;
+  public sidebarVisibility: boolean = false;
+  zIndex!: number;
+
+  constructor(private zIndexService: ZIndexService) {}
+
+  ngOnInit() {
+    this.zIndexService.setZIndex('navbar', 10);
+    this.zIndex = this.zIndexService.getZIndex('navbar');
+  }
 
   toggleSidebar() {
-    this.sidebarVisibility = !this.sidebarVisibility;
+    this.sidebarVisibility = !this.sidebarVisibility
+    setTimeout(() => {
+      const options = document.querySelectorAll('.option');
+      options.forEach((element:Element) => {
+        const option = element as HTMLElement;
+        option.style.opacity = this.sidebarVisibility ? '1' : '0';
+      })
+    })
   }
 
   isSidebarVisible() {
     return this.sidebarVisibility;
   }
-
 }
