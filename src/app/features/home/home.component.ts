c05b22af416dce74997568af80ad62fdef49099e
JeredHuck <jeredhuck@gmail.com>
2024-10-28 15:35:01 -0500

    started a modal for the creation of lists

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { ZIndexService } from '../../core/guards/services/z-index-service';
 import { NgStyle } from '@angular/common';
+import { ModalComponent } from './modal/modal.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgStyle],
+  imports: [NgStyle, ModalComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent implements OnInit {
   zIndex!: number;
+  showModal = false;
 
   topGames: string[] = ['Zelda', 'Mario', 'Sonic'];
 
@@ -19,5 +21,9 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
     this.zIndexService.setZIndex('home', 5);
     this.zIndex = this.zIndexService.getZIndex('home');
+  }
+
+  openModal() {
+    this.showModal = true;
   }
 }
---
+++ src/app/features/home/modal/modal.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.scss'
+})
+export class ModalComponent {
+  @Input() visibility = false;
+
+  closeModal() {
+    this.visibility = false;
+  }
+}
