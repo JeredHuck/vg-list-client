7ee63ec224df9a630f6f3863c9f57ee86ab14865
JeredHuck <jeredhuck@gmail.com>
2024-11-04 11:51:46 -0600

    began adding form to the home page modal

--- src/app/features/home/modal/modal.component.ts
+++ src/app/features/home/modal/modal.component.ts
@@ -1,14 +1,23 @@
 import { Component, Inject } from '@angular/core';
+import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-modal',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.scss',
 })
 export class ModalComponent {
+  gameList = new FormGroup({
+    game1: new FormControl(''),
+    game2: new FormControl(''),
+    game3: new FormControl(''),
+    game4: new FormControl(''),
+    game5: new FormControl(''),
+  });
+
   constructor(
     public dialogRef: MatDialogRef<ModalComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
